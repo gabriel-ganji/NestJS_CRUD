40ca5d78604bc08897ed9ec843ec16a727ac3663
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-04-07 18:26:21 -0300

    Creating Service/Provider

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -9,14 +9,19 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MessageService } from './message.service';
+
 @Controller('message')
 export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
   //Route to find all messages
   @HttpCode(201)
   @Get()
   findAll(@Query() pagination: any): string {
     const { limit = 10, offset = 0 } = pagination;
-    return `This route return all message. Limit=${limit} and offset=${offset}`;
+    // return `This route return all message. Limit=${limit} and offset=${offset}`;
+    return this.messageService.hello();
   }
 
   //Route to find one message
