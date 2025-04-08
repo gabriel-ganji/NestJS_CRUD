72f67dac8a0675c11bf33b8387efdf70effba05f
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-04-08 13:30:01 -0300

    feat: useGlobalPipes, ValidationPipe and class-validator

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,5 +1,30 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString({
+    message: 'Error! You need send me a string.',
+  })
+  @IsNotEmpty()
+  @MinLength(3)
   readonly text: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(255)
+  @IsOptional()
   readonly from: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(255)
+  @IsOptional()
   readonly to: string;
 }
