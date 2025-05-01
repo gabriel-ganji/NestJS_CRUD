75f01cbb6605d4928e67533204ca180f61660e06
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-05-01 15:50:03 -0300

    feat(interceptors): dependency injection

--- src/common/interceptors/error-handling.interceptor.ts
+++ src/common/interceptors/error-handling.interceptor.ts
@@ -2,11 +2,13 @@ import {
   BadRequestException,
   CallHandler,
   ExecutionContext,
+  Injectable,
   NestInterceptor,
 } from '@nestjs/common';
 import { catchError, throwError } from 'rxjs';
 // import { Observable } from 'rxjs';
 
+@Injectable()
 export class ErrorHandlingInterceptor implements NestInterceptor {
   async intercept(context: ExecutionContext, next: CallHandler<any>) {
     // console.log('ErrorHandlingInterceptor executed. BEFORE!');
